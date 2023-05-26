af8b7759dd045613f2ee92a30dbdec4b4c389cc9
Javier Sanchez B <ing.javier.sanchez.b@gmail.com>
2023-05-25 21:40:23 -0500

    api mongodb

--- app.js
+++ app.js
@@ -11,6 +11,26 @@ const client = new MongoClient(url);
 // Database Name
 const dbName = process.env.DBNAME;
 
+
+exports = ({ token, tokenId, username, password, currentPasswordValid }) => {
+  // check if the username corresponds to a real user
+  const isUser = myCustomValidator.validate(username);
+  // check if the user is attempting to reset their password to their current password
+  if (currentPasswordValid) {
+    myCustomNotifier.sendMessage(username, "Cannot reset password to current password.");
+    return { status: 'fail' };
+  }
+  // check if the user has requested a password reset too often recently
+  const isNotCoolingDown = myCustomCooldownService.canReset(username, 'myCustomService')
+  // send a message to the user in some way so that the user can confirm themselves
+  const msgSendSuccessful = isUser && isNotCoolingDown && myCustomMsgr.send(username, token, tokenId)
+  if ( msgSendSuccessful ) {
+     return { status: 'pending' };
+  } else {
+     return { status: 'fail' };
+  }
+}
+
 async function main() {
   // Use connect method to connect to the server
   await client.connect();
